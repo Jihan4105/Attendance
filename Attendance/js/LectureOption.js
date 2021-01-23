d729f33ef876f08372a1b816870f2f0637301628
Jihan4105 <wlgks6979@naver.com>
2021-01-23 23:21:31 +0900

    2021_1_23 Attendance_Fixed

--- Attendance/js/LectureOption.js
+++ Attendance/js/LectureOption.js
@@ -6,7 +6,7 @@
     else {
         var checkedStates = [$("#no1").is(":checked"), $("#no2").is(":checked"), $("#no3").is(":checked"), $("#no4").is(":checked")];
         var alerts=[];
-        for (var i = 0; i < 4; i++) {
+        for (var i = 0; i < length.alerts; i++) {
             if (checkedStates[i] == true) {
                 alerts.push(i + 1);
             }
@@ -33,3 +33,12 @@ function GoFixSite() {
 function GoAddSite() {
     $("#mainBase").load("./html/lecture_AddPage.html #lectureadd")
 }
+
+$(document).ready(function () {
+    $('#lecturelist tr').click(function (event) {
+        if (event.target.nodeName.toLowerCase() == 'td') {
+            var checkbox = $(this).find('td:first-child :checkbox');
+            checkbox.attr('checked', !checkbox.is(':checked'));
+        }
+    });
+});
