9b877a2c2031673c7b717bc7c68acddd55f0faf7
Jihan4105 <wlgks6979@naver.com>
2021-01-23 14:26:40 +0900

    first

---
+++ Attendance/js/LectureOption.js
@@ -0,0 +1,35 @@
+﻿function LectureSelectFix() {
+    var isNumberChk = $("input:checkbox[name=no]").is(":checked");
+    if (!isNumberChk) {
+        alert("수정할 항목을 선택해주세요.");
+    }
+    else {
+        var checkedStates = [$("#no1").is(":checked"), $("#no2").is(":checked"), $("#no3").is(":checked"), $("#no4").is(":checked")];
+        var alerts=[];
+        for (var i = 0; i < 4; i++) {
+            if (checkedStates[i] == true) {
+                alerts.push(i + 1);
+            }
+        }
+        alert(alerts + "번 을 선택하셨습니다");
+        GoFixSite();
+    }
+}
+
+function LectureSelectDelete(){
+    var isNumberChk = $("input:checkbox[name=no]").is(":checked");
+    if (!isNumberChk) {
+        alert("삭제할 항목을 선택해주세요.");
+    }
+    else {
+        alert("선택된 항목들이 삭제되었습니다!");
+    }
+}
+
+function GoFixSite() {
+    $("#mainBase").load("./html/lecture_Fixpage.html #lecturefix");
+}
+
+function GoAddSite() {
+    $("#mainBase").load("./html/lecture_AddPage.html #lectureadd")
+}
---
+++ Attendance/js/main.js
@@ -0,0 +1,39 @@
+﻿var sideNavTopPosition;
+
+$(function () {
+    $(window).resize(function () {
+        if ($("body").width() <= 576) {
+            $("#sideNav").hide();
+            $(".main").css("margin-left", "0px");
+            $(".footer").css("margin-left", "0px");
+        }
+        else {
+            $("#sideNav").show();
+            $(".main").css("margin-left", "200px");
+            $(".footer").css("margin-left", "200px");
+        }
+    });
+    $("#sideNav").css("margin-top", $("#topNav").outerHeight());
+})();
+
+function sideNavShowHide() {
+    if ($("#sideNav").css("display") == "none") {
+        $("#sideNav").show();
+    } else {
+        $("#sideNav").hide();
+    }
+}
+
+function submenuShowHide() {
+    $("#dropdown-btn").toggleClass("active");
+    var dropdownContent = $("#dropdown-container");
+    if (dropdownContent[0].style.display === "block") {
+        dropdownContent[0].style.display = "none";
+    } else {
+        dropdownContent[0].style.display = "block";
+    }
+}
+
+function LectureSearch() {
+    $("#mainBase").load("./html/lecture_Search.html #lectureSearching");
+}
