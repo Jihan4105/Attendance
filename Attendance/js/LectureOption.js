bcbddd1f6f11067e028de116f233e545fb81cf72
Jihan4105 <wlgks6979@naver.com>
2021-01-27 21:06:23 +0900

    2021.1.27 Attdendance

--- Attendance/js/LectureOption.js
+++ Attendance/js/LectureOption.js
@@ -1,4 +1,4 @@
-﻿function LectureSelectFix() {
+﻿function LectureLookup() {
     var deferred = $.Deferred();
     $.ajax({
         url: "./WebService/WebService.asmx/HelloWorld",
@@ -7,15 +7,14 @@
         method: "post",
         success: function (result) {
             var html = "";
-            for (var i = 0; i < 10;i++) {
-                html += '<tr>'
-                html += '<td>' + items[No].No + '</td>'
-                html += '<td>' + items[No].className + '</td>'
-                html += '<td>' + items[No].time + '</td>'
-                html += '<td>' + items[No].proFessor + '</td>'
-                html += '</tr>'
+            for (No in result.items) {
+                html += '<tr>';
+                html += '<td>' + result.items[No].No + '.' + '<input type="checkbox" name="no[]">' + '</td>';
+                html += '<td>' + result.items[No].className + '</td>';
+                html += '<td>' + result.items[No].time + '</td>';
+                html += '<td>' + result.items[No].proFessor + '</td>';
+                html += '</tr>';
             }
-            html += '</tbody>' + '</table>'
             $("#lecturelist").empty();
             $("#lecturelist").append(html);
             deferred.resolve(result);
@@ -26,16 +25,27 @@
     });
 }
 
-function LectureSelectDelete() {
-    var isNumberChk = $("input:checkbox[name=no]").is(":checked");
-    if (!isNumberChk) {
-        alert("삭제할 항목을 선택해주세요.");
+function DeleteButton() {
+    /*var isNumChk = $("input:checkbox[name='no[]']").is(":checked");
+    var arr_num = document.getElementsByName("no[]");
+    if (!isNumChk) {
+        alert("한개 이상의 삭제할 항목을 선택해 주십시오.");
+        return false;
     }
     else {
-        alert("선택된 항목들이 삭제되었습니다!");
-    }
+        for (var i = 0; i < arr_num.length; i++) {
+            if (arr_num[i].checked == true) {
+                $.ajax({
+                    url: './WebService/WebService.asmx/HelloWorld' ,
+                    type: "GET",
+                    dataType: "JSON"
+                }).done(function (response) {
+                    $(this).parents+
+                });
+            }
+        }
+    }*/
 }
-
 function GoFixSite() {
     $("#mainBase").load("./html/lecture_Fixpage.html #lecturefix");
 }
@@ -43,12 +53,3 @@ function GoFixSite() {
 function GoAddSite() {
     $("#mainBase").load("./html/lecture_AddPage.html #lectureadd")
 }
-
-$(document).ready(function () {
-    $('#lecturelist tr').click(function (event) {
-        if (event.target.nodeName.toLowerCase() == 'td') {
-            var checkbox = $(this).find('td:first-child :checkbox');
-            checkbox.attr('checked', !checkbox.is(':checked'));
-        }
-    });
-});
