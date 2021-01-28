0d5797225a4b3d247baadb04c7c2475a0e7fb40b
Jihan4105 <wlgks6979@naver.com>
2021-01-28 18:58:41 +0900

    2021.01.28 Attendnace (succed: deleting and renum table)

--- Attendance/js/LectureOption.js
+++ Attendance/js/LectureOption.js
@@ -9,7 +9,7 @@
             var html = "";
             for (No in result.items) {
                 html += '<tr>';
-                html += '<td>' + result.items[No].No + '.' + '<input type="checkbox" name="no[]">' + '</td>';
+                html += '<td class="row-id">' + result.items[No].No + '.' + '<input type="checkbox" name="no">' + '</td>';
                 html += '<td>' + result.items[No].className + '</td>';
                 html += '<td>' + result.items[No].time + '</td>';
                 html += '<td>' + result.items[No].proFessor + '</td>';
@@ -26,21 +26,28 @@
 }
 
 function DeleteButton() {
-    var isNumChk = $("input:checkbox[name='no[]']").is(":checked");
+    var isNumChk = $("input:checkbox[name='no']").is(":checked");
     var my_tbody = document.getElementById("lecturelist");
-    var arr_num = document.getElementsByName("no[]");
+    var nom = document.getElementsByName("no");
     if (!isNumChk) {
         alert("한개 이상의 삭제할 항목을 선택해 주십시오.");
         return false;
     }
     else {
-        for (var i = 0; i < arr_num.length; i++) {
-            if (arr_num[i].checked == true) {
-                my_tbody.deleteRow(my_tbody.rows.i);
+        for (var i = 0; i < nom.length; i++) {
+            if (nom[i].checked == true) {
+                my_tbody.deleteRow(i);
+                i--;
             }
         }
+        $(".row-id").each(function (i) {
+            $(this).text(i + 1);
+            $(this).append('.' + '<input type="checkbox" name="no">');
+        });
+        alert("성공적으로 삭제되었습니다!");
     }
 }
+
 function GoFixSite() {
     $("#mainBase").load("./html/lecture_Fixpage.html #lecturefix");
 }
